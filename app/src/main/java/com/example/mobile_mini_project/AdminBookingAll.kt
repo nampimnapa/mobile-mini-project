5da7c51ce0b10f62ba23bf604098b494ff7a2fc6
nampimnapa <pimnapa.s@kkumail.com>
2023-03-01 16:39:06 +0700

    update 01/03/2023 ครั้งที่ 1

---
+++ app/src/main/java/com/example/mobile_mini_project/AdminBookingAll.kt
@@ -0,0 +1,11 @@
+package com.example.mobile_mini_project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AdminBookingAll : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_admin_booking_all)
+    }
+}
---
+++ app/src/main/java/com/example/mobile_mini_project/UserShowCar.kt
@@ -0,0 +1,11 @@
+package com.example.mobile_mini_project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UserShowCar : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_show_car)
+    }
+}
